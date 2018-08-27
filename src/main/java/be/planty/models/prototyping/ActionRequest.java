d2d4240f23f35a315f6c7551748c1e588d45b3b3
Hadi Ahmadi <MacPersia@gmail.com>
2018-08-28 02:04:01 +0800

    Added parameters to model

--- src/main/java/be/planty/models/prototyping/ActionRequest.java
+++ src/main/java/be/planty/models/prototyping/ActionRequest.java
@@ -2,12 +2,20 @@ package be.planty.models.prototyping;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Collections.unmodifiableMap;
+
 public class ActionRequest {
 
     public final String action;
+    public final Map<String, String> parameters;
 
     @JsonCreator
-    public ActionRequest(String action) {
+    public ActionRequest(String action, Map<String, String> parameters) {
         this.action = action;
+        this.parameters = unmodifiableMap(parameters);
     }
 }
