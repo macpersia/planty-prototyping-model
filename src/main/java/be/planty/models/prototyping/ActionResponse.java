4b501256b34764e095c2afa8b4c972899f7b01c9
macpersia <macpersia@gmail.com>
2018-09-27 12:56:24 +0800

    Added body to action response

--- src/main/java/be/planty/models/prototyping/ActionResponse.java
+++ src/main/java/be/planty/models/prototyping/ActionResponse.java
@@ -3,10 +3,17 @@ package be.planty.models.prototyping;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class ActionResponse extends be.planty.models.assistant.ActionResponse {
+public class ActionResponse<T> extends be.planty.models.assistant.ActionResponse {
+
+    public final T body;
 
     @JsonCreator
     public ActionResponse(@JsonProperty("statusCode") Integer statusCode) {
+        this(statusCode, null);
+    }
+
+    public ActionResponse(Integer statusCode, T body) {
         super(statusCode);
+        this.body = body;
     }
 }
